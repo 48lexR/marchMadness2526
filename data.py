744795c07539ab36262a74cc8671afb126943071
cage <cbull358@gmail.com>
2025-11-17 13:25:47 -0500

    Added functionality

--- data.py
+++ data.py
@@ -12,23 +12,36 @@ class Data:
         self._data = self._data.sort_values(_sort_axis_label, ascending=True)
         self._siz = _siz
 
-    def __call__(self):
-        labels = list(self._data["TeamName"])
-        x = list(self._data["AdjOE"])
-        y = list(self._data["AdjDE"])
+    def __call__(self, _xaxis = "AdjOE", _yaxis = "AdjDE") -> None:
+        labels = self._data["TeamName"]
+        ranks = self._data["RankAdjEM"]
+        x_vals = self._data[_xaxis]
+        y_vals = self._data[_yaxis]
 
         if(self._siz != -1):
             labels = labels[:self._siz]
-            x = x[:self._siz]
-            y = y[:self._siz]
+            ranks = ranks[:self._siz]
+            x_vals = x_vals[:self._siz]
+            y_vals = y_vals[:self._siz]
 
-        plt.xlabel("Adjusted Offensive Efficiency")
-        plt.ylabel("Adjusted Defensive Efficiency")
+        plt.xlabel(_xaxis)
+        plt.ylabel(_yaxis)
         plt.title("Offensive Efficiency (Adj) vs Defensive Efficiency (Adj) (KenPom Top 50)")
-        plt.scatter(x, y)
+        plt.scatter(x_vals, y_vals)
+        ax = plt.gca()
 
-        for i, txt in enumerate(labels):
-            plt.text(x[i] + 0.1, y[i] + 0.2, txt, fontsize=9)
+        if _xaxis == "AdjDE":
+            ax.invert_xaxis()
+        elif _yaxis == "AdjDE":
+            ax.invert_yaxis()
+
+        for i, (x, y, label, rank) in enumerate(zip(x_vals, y_vals, labels, ranks)):
+            print(f"{label} ({rank})")
+            plt.text(x + 0.1, y + 0.2, f"{label} ({rank})", fontsize=8)
 
         plt.grid(True)
         plt.show()
+
+    def getOptions(self) -> list[str]:
+        return [i for i in self._data.columns]
+            
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from data import Data
 
 def main():
     data = Data("summary26.csv", _siz=50)
-    data()
+    print(f"Axis options")
+    for s in data.getOptions(): print(s)
+    xaxis=input("Input an axis:\n")
+    yaxis=input("Input an axis:\n")
+    data(_xaxis=xaxis, _yaxis=yaxis)
 
 if __name__ == "__main__":
     main()
