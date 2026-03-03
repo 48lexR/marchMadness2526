971e7ebfbec11800b7d887175df134660bb833b4
Cage <cbull358@gmail.com>
2026-03-02 22:28:06 -0500

    Improvements

--- data.py
+++ data.py
@@ -64,12 +64,15 @@ class Data:
         turnover_factor_A = self.predict_turnover_factor(teamA, teamB)
         turnover_factor_B = self.predict_turnover_factor(teamB, teamA)
         tempo = max(teamA["Tempo"], teamB["Tempo"])
+        P_A = 
         # NOTE: We add the shooting factor instead of multiplying because one FT = 1 pt (not 2). FTA is not included in FGM/eFG
-        expectation_A = (2+shooting_factor_A) * tempo * (teamA["O-eFGPct"]/100) * turnover_factor_A * rebounding_factor_A
+        expectation_A = (2+shooting_factor_A) * (teamA["O-eFGPct"]/100) * turnover_factor_A * rebounding_factor_A
+        variance_A = expectation_A ** 2 - (turnover_factor_A * rebounding_factor_A)**2 * (2 + shooting_factor_A) * (teamA["O-eFGPct"]/100)
         # print(f"{teamNameA}\t{shooting_factor_A}\t{turnover_factor_A}\t{rebounding_factor_A}\t{expectation_A}")
         expectation_B = (2+shooting_factor_B) * tempo * (teamB["O-eFGPct"]/100) * turnover_factor_B * rebounding_factor_B
+        variance_B = expectation_B ** 2 - (turnover_factor_B * rebounding_factor_B)**2 * (2 + shooting_factor_B) * (teamB["O-eFGPct"]/100)
         # print(f"{teamNameB}\t{shooting_factor_B}\t{turnover_factor_B}\t{rebounding_factor_B}\t{expectation_B}")
-        return f"{teamNameA}: {expectation_A}\r\n{teamNameB}: {expectation_B}\r\n"
+        return f"{teamNameA}: {expectation_A}\r\nVariation: {variance_A}\r\n{teamNameB}: {expectation_B}\r\nVariance: {variance_B}\r\n"
 
     def predict_shooting_factor(self, teamA, teamB):
         team_A_offensive_efg = teamA["O-FTRate"] / 100
